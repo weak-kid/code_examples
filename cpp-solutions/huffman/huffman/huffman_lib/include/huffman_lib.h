677c81aec622b715d442a415ddee4dd4d9971f6b
Nekrasov Alexander <367430@niuitmo.ru>
2025-08-04 00:19:15 +0300

    proper version

--- cpp-solutions/huffman/huffman/huffman_lib/include/huffman_lib.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "counter.h"
-#include "huffman_tree.h"
-#include "serialization.h"
-
-namespace huffman {
-void compress(std::istream& in, std::ostream& out);
-void decompress(std::istream& in, std::ostream& out);
-} // namespace huffman
